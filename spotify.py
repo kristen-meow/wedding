44edc86df0cceb3a23b23cedfc3e73fc4bbb65c9
Kristen Reale <kristenmreale@gmail.com>
2024-07-06 13:42:00 -0400

    updates to code

--- spotify.py
+++ spotify.py
@@ -1,2 +1,14 @@
 import pandas as pd
+import requests
+import numpy as np
+import json
 
+
+url = "https://api.spotify.com/v1/playlists/6sJaRzQ7VJ5iNG4NYo58Db/tracks?fields=items(track(name)%2Ctrack(artists(name)))"
+
+headers = {
+
+}
+
+track_name = []
+artist_name = []
